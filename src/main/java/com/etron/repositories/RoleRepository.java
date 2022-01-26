1e171eaa6ef6165960d0fdc0ad298281892540dd
EssalehiZainaba <esszainaba@gmail.com>
2022-01-26 18:00:10 +0100

    admin

--- src/main/java/com/etron/repositories/RoleRepository.java
+++ src/main/java/com/etron/repositories/RoleRepository.java
@@ -1,7 +1,12 @@
 package com.etron.repositories;
 
-import com.etron.models.Role;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.etron.models.Role;
+import com.etron.models.enums.AppRole;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByAppRole(AppRole appRole);
 }
--- src/main/java/com/etron/repositories/UserRepository.java
+++ src/main/java/com/etron/repositories/UserRepository.java
@@ -1,7 +1,14 @@
 package com.etron.repositories;
 
-import com.etron.models.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.etron.models.User;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
+
+	boolean existsByEmail(String email);
+
 }
