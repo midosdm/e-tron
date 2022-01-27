99b2264ca9f6536a3d7009fe171a3a5e1ae7cb04
EssalehiZainaba <esszainaba@gmail.com>
2022-01-27 16:15:31 +0100

    users

--- src/main/java/com/etron/controllers/AdminController.java
+++ src/main/java/com/etron/controllers/AdminController.java
@@ -1,5 +1,90 @@
 package com.etron.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.etron.dto.AdminDto;
+import com.etron.models.Admin;
+import com.etron.services.AdminService;
+
+@RestController
+@RequestMapping(path = "/api/v1/admins")
+
 public class AdminController {
+
+	@Autowired
+	private AdminService adminService;
+
+	private ModelMapper modelmapper;
+
+	private AdminDto convertToDto(Admin admin) {
+		return modelmapper.map(admin, AdminDto.class);
+	}
+
+	private Admin convertToEntity(AdminDto adminDto) {
+		return modelmapper.map(adminDto, Admin.class);
+	}
+
+	private Admin convertToEntity(AdminWithoutPasswordDto adminDto) {
+		return modelmapper.map(adminDto, Admin.class);
+	}
+
+	@GetMapping
+	public List<AdminDto> getAllAdmins() {
+		List<Admin> adminList = adminService.getAllAdmins().getBody();
+		return adminList.stream().map(this::convertToDto).collect(Collectors.toList());
+	}
+
+	@GetMapping(path = "{adminId}")
+	public AdminDto getAdminById(@PathVariable("adminId") Long adminId) {
+		var admin = adminService.getAdminById(adminId).getBody();
+		return convertToDto(admin);
+	}
+
+	@PostMapping
+	public AdminDto createAdmin(@Valid @RequestBody AdminDto newAdminDto) {
+		var admin = convertToEntity(newAdminDto);
+		var newAdmin = adminService.createAdmin(admin).getBody();
+		return convertToDto(newAdmin);
+	}
+
+	@PutMapping(path = "{adminId}")
+	public AdminDto updateAdmin(@PathVariable("adminId") Long adminId,
+			@Valid @RequestBody AdminWithoutPasswordDto adminDto) {
+		var admin = convertToEntity(adminDto);
+		var newAdmin = adminService.updateAdmin(adminId, admin).getBody();
+		return convertToDto(newAdmin);
+	}
+
+	@PutMapping(path = "{adminId}/password")
+	public void updatePassword(@PathVariable("adminId") Long adminId,
+			@Valid @RequestBody UserPasswordDto userPasswordDto) {
+		adminService.updatePassword(adminId, userPasswordDto.getPassword());
+	}
+
+	@DeleteMapping(path = "{adminId}")
+	public void deleteAdmin(@PathVariable("adminId") Long adminId) {
+		adminService.deleteAdmin(adminId);
+	}
+
+	@DeleteMapping
+	public ResponseEntity<HttpStatus> deleteAllAdmins() {
+		return adminService.deleteAllAdmins();
+	}
 
 }
--- src/main/java/com/etron/controllers/SubscriberController.java
+++ src/main/java/com/etron/controllers/SubscriberController.java
@@ -1,5 +1,90 @@
 package com.etron.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.etron.dto.SubscriberDto;
+import com.etron.models.Subscriber;
+import com.etron.services.SubscriberService;
+
+@RestController
+@RequestMapping(path = "/api/v1/subscribers")
+
 public class SubscriberController {
+
+	@Autowired
+	private SubscriberService subscriberService;
+
+	private ModelMapper modelmapper;
+
+	private SubscriberDto convertToDto(Subscriber subscriber) {
+		return modelmapper.map(subscriber, SubscriberDto.class);
+	}
+
+	private Subscriber convertToEntity(SubscriberDto subscriberDto) {
+		return modelmapper.map(subscriberDto, Subscriber.class);
+	}
+
+	private Subscriber convertToEntity(SubscriberWithoutPasswordDto subscriberDto) {
+		return modelmapper.map(subscriberDto, Subscriber.class);
+	}
+
+	@GetMapping
+	public List<SubscriberDto> getAllSubscribers() {
+		List<Subscriber> subscriberList = subscriberService.getAllSubscribers().getBody();
+		return subscriberList.stream().map(this::convertToDto).collect(Collectors.toList());
+	}
+
+	@GetMapping(path = "{subscriberId}")
+	public SubscriberDto getSubscriberById(@PathVariable("subscriberId") Long subscriberId) {
+		var subscriber = subscriberService.getSubscriberById(subscriberId).getBody();
+		return convertToDto(subscriber);
+	}
+
+	@PostMapping
+	public SubscriberDto createSubscriber(@Valid @RequestBody SubscriberDto newSubscriberDto) {
+		var subscriber = convertToEntity(newSubscriberDto);
+		var newSubscriber = subscriberService.createSubscriber(subscriber).getBody();
+		return convertToDto(newSubscriber);
+	}
+
+	@PutMapping(path = "{subscriberId}")
+	public SubscriberDto updateAdmin(@PathVariable("adminId") Long subscriberId,
+			@Valid @RequestBody SubscriberWithoutPasswordDto subscriberDto) {
+		var subscriber = convertToEntity(subscriberDto);
+		var newSubscriber = subscriberService.updateSubscriber(subscriberId, subscriber).getBody();
+		return convertToDto(newSubscriber);
+	}
+
+	@PutMapping(path = "{subscriberId}/password")
+	public void updatePassword(@PathVariable("subscriberId") Long subscriberId,
+			@Valid @RequestBody UserPasswordDto userPasswordDto) {
+		subscriberService.updatePassword(subscriberId, userPasswordDto.getPassword());
+	}
+
+	@DeleteMapping(path = "{subscriberId}")
+	public void deleteAdmin(@PathVariable("subscriberId") Long subscriberId) {
+		subscriberService.deleteSubscriber(subscriberId);
+	}
+
+	@DeleteMapping
+	public ResponseEntity<HttpStatus> deleteAllAdmins() {
+		return subscriberService.deleteAllSubscribers();
+	}
 
 }
